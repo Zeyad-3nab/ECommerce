b18b516454798a556cdef0efa522f59e41ea383a
Zeyad <zeyadenab220@gmail.com>
2024-07-20 19:04:05 +0300

    Test

--- Ecommerce/Program.cs
+++ Ecommerce/Program.cs
@@ -11,6 +11,7 @@ namespace Ecommerce
     {
         public static void Main(string[] args)
         {
+            //Test
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
